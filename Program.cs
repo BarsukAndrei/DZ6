0074b5ce118ea82f46d2aa813aff744039315f89
Andrei <narsuk-00@inbox.ru>
2023-07-11 19:37:28 +0300

    Решение задач 6 задания

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+
+/* int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] InputArray(int length);
+{
+    int[] array = new int(length);
+    for(int i = 0; i < array.Leagth; i++)
+    {
+        array[i] = Prompt($"Введите (i+1)-й элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Leagth; i++);
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Leagth; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++
+        }
+    }
+    return count;
+}
+
+int length = Prompt("Введите количество элементов > ");
+int[] array;
+array = InputArray(length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(array)}");
+*/
